1b7bb2eb6afda58b3a22c747e0554a7b8bf9c42d
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-02-26 18:05:11 +0100

    Initial commit

---
+++ gaussnet.py
@@ -0,0 +1,13 @@
+########################################################################################################################
+#                                                                                                                      #
+#                     GaussNet: Solving the electrostatic Poisson equation for plasma simulations                      #
+#                                                                                                                      #
+#                         Guillaume Bogopolsky, Lionel Cheng, Ekhi Ajuria, CERFACS, 26.02.2020                         #
+#                                                                                                                      #
+########################################################################################################################
+
+# Main program
+
+import os
+import argparse
+import yaml
