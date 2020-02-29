f6f4d41e6aebed4af4eda03d7789170c6f3d1c11
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-02-29 22:43:24 +0100

    Add divergence operator and test (bug on corners?)

--- PlasmaNet/Operators/divergence.py
+++ PlasmaNet/Operators/divergence.py
@@ -1,8 +1,8 @@
 ########################################################################################################################
 #                                                                                                                      #
-#                                         Define divergence function and mask                                          #
+#                                                 Divergence operator                                                  #
 #                                                                                                                      #
-#                        Ekhi Ajuria, Guillaume Bogopolsky (transcription) CERFACS, 27.02.2020                         #
+#                               Lionel Cheng, Guillaume Bogopolsky, CERFACS, 28.02.2020                                #
 #                                                                                                                      #
 ########################################################################################################################
 
@@ -10,15 +10,16 @@
 import torch
 
 
-def divergence_mask(field, dx, dy):
+def divergence(field, dx, dy):
     """
-    Calculates the electric field divergence (with boundary condition modifications). This is essentially a replica
-    of makeRhs in Manta and FluidNet.
+    Calculates the field divergence (second order accurate, degraded to first order on boundaries).
+    The output shape is the same as the input shape.
 
     Parameters
     ----------
     field : torch.Tensor
         Input 2D field: tensor of size (batch, 2, H, W)
+
     dx, dy : float
 
     Returns
@@ -27,84 +28,41 @@ def divergence_mask(field, dx, dy):
         Output divergence (scalar field)
     """
 
-    # Check sizes
+    # Create divergence tensor with shape (batch_size, h, w)
     divergence = torch.zeros_like(field[:, 0]).type(field.type()).unsqueeze(1)
+
+    # Check sizes
     assert field.dim() == 4 and divergence.dim() == 4, 'Dimension mismatch'
-    assert field.size(1) == 2, 'E is not 2D'
+    assert field.size(1) == 2, 'field is not 2D'
     batch_size = field.size(0)
     h, w = field.size(2), field.size(3)
 
     assert field.is_contiguous() and divergence.is_contiguous(), 'Input is not contiguous'
 
-    # Define work vectors
-    field_ij_p = torch.zeros_like(field)  # Field in ij
-    field_ij_n = torch.zeros_like(field)
-    field_ij = field.clone()
+    # Compute divergence
+    # i <-> y and j <-> x, so field[:, 0, :, :] is field_x and field[:, 1, :, :] is field_y
+    # center of the array
+    divergence[:, 0, 1:-1, 1:-1] = (field[:, 0, 1:-1, 2:] - field[:, 0, 1:-1, :-2]) / (2 * dx) + \
+                                   (field[:, 1, 2:, 1:-1] - field[:, 1, :-2, 1:-1]) / (2 * dy)
 
-    # Create translated work tensors
-    field_ij_p[:, 0, :, 1:] = field_ij[:, 0, :, :-1]  # Copy field_x with padding 1 along y
-    field_ij_p[:, 0, :, 0] = field_ij[:, 0, :, 0]  # Copy 1st value (TODO duplicated on 1st and 2nd y positions?)
-    field_ij_p[:, 1, 1:, :] = field_ij[:, 1, :-1, :]  # Copy field_y with padding 1 along x
-    field_ij_p[:, 1, 0, :] = field_ij[:, 1, 0, :]  # Copy 1st value (TODO duplicated on 1st and 2nd x positions?)
+    #  array sides except corners (respectively upper, lower, left and right sides)
+    divergence[:, 0, 0, 1:-1] = (field[:, 0, 0, 2:] - field[:, 0, 0, :-2]) / (2 * dx) + \
+                                (field[:, 1, 1, 1:-1] - field[:, 1, 0, 1:-1]) / dy
+    divergence[:, 0, -1, 1:-1] = (field[:, 0, -1, 2:] - field[:, 0, -1, :-2]) / (2 * dx) + \
+                                 (field[:, 1, -1, 1:-1] - field[:, 1, -2, 1:-1]) / dy
+    divergence[:, 0, 1:-1, 0] = (field[:, 0, 1:-1, 1] - field[:, 0, 1:-1, 0]) / dx + \
+                                (field[:, 1, 2:, 0] - field[:, 1, :-2, 0]) / (2 * dy)
+    divergence[:, 0, 1:-1, -1] = (field[:, 0, 1:-1, -1] - field[:, 0, 1:-1, -2]) / dx + \
+                                 (field[:, 1, 2:, -1] - field[:, 1, :-2, -1]) / (2 * dy)
 
-    field_ij_n[:, 0, :, :-1] = field_ij[:, 0, :, 1:]  # Copy field_x with -1 padding along y
-    field_ij_n[:, 0, :, -1] = field_ij[:, 0, :, -1]  # Copy last value (TODO duplicated on nth and n-1 positions?)
-    field_ij_n[:, 1, :-1, :] = field_ij[:, 1, 1:, :]
-    field_ij_n[:, 1, -1, :] = field_ij[:, 1, -1, :]
-
-    # Finite element derivative
-    div_center = (field_ij_n.select(1, 0) - field_ij_p.select(1, 0)) / (2 * dx) + \
-                 (field_ij_n.select(1, 1) - field_ij_p.select(1, 1)) / (2 * dy)
-    div_fwd = (field_ij_n.select(1, 0) - field_ij.select(1, 0)) / dx + \
-              (field_ij_n.select(1, 1) - field_ij.select(1, 1)) / dy
-    div_back = (field_ij.select(1, 0) - field_ij_p.select(1, 0)) / dx + \
-               (field_ij.select(1, 1) - field_ij_p.select(1, 1)) / dy
-
-    # Indexes and masks
-    indexes_x = ((torch.arange(w)).view(1, w)).expand(h, w).unsqueeze(0)
-    indexes_y = ((torch.arange(h)).view(h, 1)).expand(h, w).unsqueeze(0)
-
-    indexes = torch.zeros((2, h, w))
-    indexes[0], indexes[1] = indexes_x, indexes_y
-
-    indexes.unsqueeze(0).expand(batch_size, 2, h, w)
-
-    zero = torch.zeros_like(indexes_x).type(torch.int16)
-    ones = torch.ones_like(indexes_x).type(torch.int16)
-
-    # center_flag[0] = torch.where(indexes_x[0] > 0 and indexes_y[0] > 0 and indexes_x[0] < (w - 1)
-    # and indexes_y[0] < ones * (h - 1), ones[0], zero[0])
-    center_flag = torch.where(indexes_x[0] < (w - 1), ones[0], zero[0])
-    center_flag = torch.where(indexes_x[0] > 0, center_flag, zero[0])
-    center_flag = torch.where(indexes_y[0] > 0, center_flag, zero[0])
-    center_flag = torch.where(indexes_y[0] < (h - 1), center_flag, zero[0])
-
-    fwd_flag = torch.where(indexes_x[0] < 1, ones[0], zero[0])
-    fwd_flag = torch.where(indexes_y[0] < 1, ones[0], fwd_flag)
-
-    bwd_flag = torch.where(indexes_x[0] >= (w - 1), ones[0], zero[0])
-    bwd_flag = torch.where(indexes_y[0] >= (h - 1), ones[0], bwd_flag)
-
-    # Send flags tensor to GPU
-    center_flag, fwd_flag, bwd_flag = center_flag.cuda(), fwd_flag.cuda(), bwd_flag.cuda()
-
-    # Compute gradient
-    div = div_center.masked_fill(center_flag.ne(1), 0) + div_fwd.masked_fill(fwd_flag.ne(1), 0) + \
-          div_back.masked_fill(bwd_flag.ne(1), 0)
-
-    div[:, -1, 0] = (field_ij_n[:, 0, -1, 0] - field_ij[:, 0, -1, 0]) / dx + \
-                    (field_ij[:, 1, -1, 0] - field_ij_p[:, 1, -1, 0]) / dy
-    div[:, 0, -1] = (field_ij[:, 0, 0, -1] - field_ij_p[:, 0, 0, -1]) / dx + \
-                    (field_ij_n[:, 1, 0, -1] - field_ij[:, 1, 0, -1]) / dy
-
-    assert field_ij_p.dim() == 4, 'Dimension mismatch in field_ij_p'
-    assert field_ij_n.dim() == 4, 'Dimension mismatch in field_ij_n'
-    assert field_ij.dim() == 4, 'Dimension mismatch in field_ij'
-
-    divergence[:, :, 0:h, 0:w] = div.view(batch_size, 1, h, w)
-
-    divergence[:, 0, :, -1] = torch.zeros((batch_size, h))
-    divergence[:, 0, :, 0] = torch.zeros((batch_size, h))
-    divergence[:, :, :, w - 2] = divergence[:, :, :, w - 3]
+    # corners (respectively upper left, upper right, lower left and lower right)
+    divergence[:, 0, 0, 0] = (field[:, 0, 0, 1] - field[:, 0, 0, 0]) / dx + \
+                             (field[:, 1, 1, 0] - field[:, 1, 0, 0]) / dy
+    divergence[:, 0, -1, 0] = (field[:, 0, -1, 1] - field[:, 0, -1, 0]) / dx +\
+                              (field[:, 1, -1, 0] - field[:, 1, -2, 0]) / dy
+    divergence[:, 0, 0, -1] = (field[:, 0, 0, -1] - field[:, 0, 0, -2]) / dx +\
+                              (field[:, 1, 1, -1] - field[:, 1, 0, -1]) / dy
+    divergence[:, 0, -1, -1] = (field[:, 0, -1, -1] - field[:, 0, -1, -2]) / dx + \
+                               (field[:, 1, -1, -1] - field[:, 1, -2, -1]) / dy
 
     return divergence
---
+++ tests/Operators/test_divergence.py
@@ -0,0 +1,60 @@
+########################################################################################################################
+#                                                                                                                      #
+#                                           Test the divergence operator                                               #
+#                                                                                                                      #
+#                               Lionel Cheng, Guillaume Bogopolsky, CERFACS, 28.02.2020                                #
+#                                                                                                                      #
+########################################################################################################################
+
+
+import torch
+import numpy as np
+import pytest
+from PlasmaNet.Operators.divergence import divergence as div
+
+
+def test_divergence_2d():
+    """ Test the divergence operator on an analytical case."""
+    xmin, xmax, ymin, ymax = 0, 1, 0, 1
+    nx, ny = 101, 101
+    nchannels = 10
+    dx, dy = (xmax - xmin) / (nx - 1), (ymax - ymin) / (ny - 1)
+    x, y = torch.linspace(xmin, xmax, nx), torch.linspace(ymin, ymax, ny)
+    Y, X = torch.meshgrid(y, x)  # Pay attention to the reversed order of the axes with torch.Tensor !
+
+    # Field and analytical solution initialisation
+    field = torch.zeros((nchannels, 2, ny, nx)).type(torch.float64)
+    analytical = torch.zeros((nchannels, 2, ny, nx)).type(torch.float64)
+    for channel in range(nchannels):
+        field[channel, 0, :, :] = X**2
+        field[channel, 1, :, :] = Y**2
+        analytical[channel, 0, :, :] = 2 * X + 2 * Y
+
+    # Compute divergence
+    computed = div(field, dx, dy)
+
+    # assert torch.all(computed.eq(analytical))
+    return X, Y, computed, analytical, field
+
+
+if __name__ == '__main__':
+    import torch
+    import matplotlib.pyplot as plt
+    from matplotlib import ticker
+
+    x, y, computed, analytical, field = test_divergence_2d()
+
+    print((computed[0, 0, :, :] - analytical[0, 0, :, :]))
+    print(torch.allclose(computed[0], analytical[0], atol=1.e-5, rtol=1.e-1))
+
+    fig, axarr = plt.subplots(2, 2, figsize=(10, 8))
+    ax1, ax2, ax3, _ = axarr.ravel()
+    p1 = ax1.contourf(x, y, analytical[0, 0, :, :], 100)
+    cbar1 = fig.colorbar(p1, label='Analytical divergence field', ax=ax1)
+    p2 = ax2.contourf(x, y, computed[0, 0, :, :], 100)
+    cbar2 = fig.colorbar(p2, label='Computed divergence field', ax=ax2)
+    p3 = ax3.contourf(x, y, torch.abs(computed[0, 0, :, :] - analytical[0, 0, :, :]) / analytical[0, 0, :, :], 100,
+                      locator=ticker.LogLocator())
+    cbar3 = fig.colorbar(p3, label='Relative difference', ax=ax3)
+    plt.tight_layout()
+    plt.show()
