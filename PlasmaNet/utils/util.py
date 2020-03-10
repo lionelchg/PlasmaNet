9533b50760c934ccb9f88803872e651466cb0043
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-03-10 18:04:36 +0100

    Add utils, MetricTracker and use yaml

--- PlasmaNet/utils/util.py
+++ PlasmaNet/utils/util.py
@@ -1,17 +1,65 @@
 ########################################################################################################################
 #                                                                                                                      #
-#                    Some utility functions (from https://github.com/victoresque/pytorch-template/)                    #
+#                                                Some utility functions                                                #
 #                                                                                                                      #
 #                                     Guillaume Bogopolsky, CERFACS, 03.03.2020                                        #
 #                                                                                                                      #
 ########################################################################################################################
 
-import json
+import yaml
+import pandas as pd
 from pathlib import Path
+from itertools import repeat
 from collections import OrderedDict
 
 
-def read_json(fname):
+def read_yaml(fname):
+    """ Read yaml configuration file. """
     fname = Path(fname)
     with fname.open('rt') as handle:
-        return json.load(handle, object_hook=OrderedDict)
+        return yaml.safe_load(handle)
+
+
+def write_yaml(content, fname):
+    """ Write yaml configuration file. """
+    fname = Path(fname)
+    with fname.open('wt') as handle:
+        yaml.safe_dump(content, handle, indent=4, sort_keys=False)
+
+
+def inf_loop(data_loader):
+    """ Wrapper function for an endless data loader. """
+    for loader in repeat(data_loader):
+        yield from loader
+
+
+class MetricTracker:
+    """
+    Track metrics from the network by storing them in a pandas.DataFrame and sending them to TensorBoard if a writer
+    is specified.
+    """
+    def __init__(self, *keys, writer=None):
+        self.writer = writer
+        self._data = pd.DataFrame(index=keys, columns=['total', 'counts', 'average'])
+        self.reset()
+
+    def reset(self):
+        """ Reset all keys to zero. """
+        for col in self._data.columns:
+            self._data[col].values[:] = 0
+
+    def update(self, key, value, n=1):
+        """ Update key to TensorBoard and in DataFrame with the given value and a count if specified. """
+        if self.writer is not None:
+            self.writer.add_scalar(key, value)
+        self._data.total[key] += value * n
+        self._data.counts[key] += n
+        self._data.average[key] = self._data.total[key] / self._data.counts[key]
+
+    def avg(self, key):
+        """ Return the average for the given key. """
+        return self._data.average[key]
+
+    def result(self):
+        """ Return averages as a dictionary. """
+        return dict(self._data.average)
