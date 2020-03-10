c95736b812789794e0b04c672e460cd4244583da
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-03-10 19:05:25 +0100

    First implementation of train and test: to check

---
+++ test.py
@@ -0,0 +1,88 @@
+########################################################################################################################
+#                                                                                                                      #
+#                                                PlasmaNet: test model                                                 #
+#                                                                                                                      #
+#                         Guillaume Bogopolsky, Lionel Cheng, Ekhi Ajuria, CERFACS, 10.03.2020                         #
+#                                                                                                                      #
+########################################################################################################################
+
+import argparse
+import torch
+from tqdm import tqdm
+import PlasmaNet.data.data_loaders as module_data
+import PlasmaNet.model.loss as module_loss
+import PlasmaNet.model.metric as module_metric
+import PlasmaNet.model.multiscalenet as module_arch
+from PlasmaNet.parse_config import ConfigParser
+
+
+def main(config):
+    logger = config.get_logger('test')
+
+    # Setup data_loader instances
+    data_loader = getattr(module_data, config['data_loader']['type'])(
+        config['data_loader']['args']['data_dir'],
+        batch_size=512,
+        shuffle=False,
+        validation_split=0.0,
+        training=False,
+        num_workers=2
+    )
+
+    # Build model architecture
+    model = config.init_obj('arch', module_arch)
+
+    # Get function handles of loss and metrics
+    loss_fn = getattr(module_loss, config['loss'])
+    metric_fns = [getattr(module_metric, metric) for metric in config['metrics']]
+
+    logger.info('Loading checkpoint: {} ...'.format(config.resume))
+    checkpoint = torch.load(config.resume)
+    state_dict = checkpoint['state_dict']
+    if config['n_gpu'] > 1:
+        model = torch.nn.DataParallel(model)
+    model.load_state_dict(state_dict)
+
+    # Prepare model for testing
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    model.eval()
+
+    total_loss = 0.0
+    total_metrics = torch.zeros(len(metric_fns))
+
+    with torch.no_grad():
+        for i, (data, target) in enumerate(tqdm(data_loader)):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+
+            #
+            # save sample images, or do something with output here
+            #
+
+            # Computing loss, metrics on test set
+            loss = loss_fn(output, target)
+            batch_size = data.shape[0]
+            total_loss += loss.item() * batch_size
+            for i, metric in enumerate(metric_fns):
+                total_metrics[i] += metric(output, target) * batch_size
+
+    n_samples = len(data_loader.sampler)
+    log = {'loss': total_loss / n_samples}
+    log.update({
+        metric.__name__: total_metrics[i].item() / n_samples for i, metric in enumerate(metric_fns)
+    })
+    logger.info(log)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PlasmaNet')
+    args.add_argument('-c', '--config', default=None, type=str,
+                      help='config file path (default: None)')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to checkpoint to resume (default: None)')
+    args.add_argument('-d', '--device', default=None, type=str,
+                      help='indices of GPUs to enable (default: all)')
+
+    config = ConfigParser.from_args(args)
+    main(config)
---
+++ train.py
@@ -0,0 +1,76 @@
+########################################################################################################################
+#                                                                                                                      #
+#                                                PlasmaNet: train model                                                #
+#                                                                                                                      #
+#                         Guillaume Bogopolsky, Lionel Cheng, Ekhi Ajuria, CERFACS, 10.03.2020                         #
+#                                                                                                                      #
+########################################################################################################################
+
+import argparse
+import collections
+import torch
+import numpy as np
+import PlasmaNet.data.data_loaders as module_data
+import PlasmaNet.model.loss as module_loss
+import PlasmaNet.model.metric as module_metric
+import PlasmaNet.model.multiscalenet as module_arch
+from PlasmaNet.parse_config import ConfigParser
+from PlasmaNet.trainer import Trainer
+
+
+# Fix random seeds for reproducibility
+SEED = 123
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(SEED)
+
+
+def main(config):
+    logger = config.get_logger('train')
+
+    # Setup data_loader instances
+    data_loader = config.init_obj('data_loader', module_data)
+    valid_data_loader = data_loader.split_validation()
+
+    # Build model architecture, then print to console
+    model = config.init_obj('arch', module_arch)
+    logger.info(model)
+
+    # Get function handles of loss and metrics
+    criterion = getattr(module_loss, config['loss'])
+    metrics = [getattr(module_metric, metric) for metric in config['metrics']]
+
+    # Build optimizer, learning rate scheduler
+    # Disable scheduler by commenting all lines with lr_scheduler
+    trainable_params = filter(lambda p: p.requires_grad(), model.parameters())
+    optimizer = config.init_obj('lr_scheduler', torch.optim, trainable_params)
+
+    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
+
+    trainer = Trainer(model, criterion, metrics, optimizer,
+                      config=config,
+                      data_loader=data_loader,
+                      valid_data_loader=valid_data_loader,
+                      lr_scheduler=lr_scheduler)
+
+    trainer.train()
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PlasmaNet')
+    args.add_argument('-c', '--config', default=None, type=str,
+                      help='config file path (default: None)')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to checkpoint to resume (default: None)')
+    args.add_argument('-d', '--device', default=None, type=str,
+                      help='indices of GPUs to enable (default: all)')
+
+    # Custom CLI options to modify configuration from default values given in yaml file
+    CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
+    options = [
+        CustomArgs(['--lr', '--learning_rate'], type=float, target='optimizer;args;lr'),
+        CustomArgs(['--bs', '--batch_size'], type=int, target='data_loader;args;batch_size')
+    ]
+    config = ConfigParser.from_args(args, options)
+    main(config)
