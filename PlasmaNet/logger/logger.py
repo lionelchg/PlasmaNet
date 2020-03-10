34fe2fdfa8d615eb3bc367ccee0735e0406025a8
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-03-10 18:03:30 +0100

    Add TensorBoard writer and add comments

--- PlasmaNet/logger/logger.py
+++ PlasmaNet/logger/logger.py
@@ -1,6 +1,6 @@
 ########################################################################################################################
 #                                                                                                                      #
-#                    Setup logging manager (from https://github.com/victoresque/pytorch-template/)                     #
+#                                                Setup logging manager                                                 #
 #                                                                                                                      #
 #                                     Guillaume Bogopolsky, CERFACS, 03.03.2020                                        #
 #                                                                                                                      #
@@ -9,14 +9,14 @@
 import logging
 import logging.config
 from pathlib import Path
-from ..utils import read_json
+from ..utils import read_yaml
 
 
 def setup_logging(save_dir, log_config='logger/logger_config.json', default_level=logging.INFO):
     """ Setup logging configuration. """
     log_config = Path(log_config)
     if log_config.is_file():
-        config = read_json(log_config)
+        config = read_yaml(log_config)
         # modify logging paths based on run config
         for _, handler in config['handlers'].items():
             if 'filename' in handler:
--- PlasmaNet/logger/visualization.py
+++ PlasmaNet/logger/visualization.py
@@ -1,6 +1,6 @@
 ########################################################################################################################
 #                                                                                                                      #
-#                     Tensorboard writer (from https://github.com/victoresque/pytorch-template/)                       #
+#                                                  Tensorboard writer                                                  #
 #                                                                                                                      #
 #                                     Guillaume Bogopolsky, CERFACS, 03.03.2020                                        #
 #                                                                                                                      #
@@ -10,7 +10,7 @@ import importlib
 from datetime import datetime
 
 
-class TensorboardWriter():
+class TensorboardWriter:
     def __init__(self, log_dir, logger, enabled):
         self.writer = None
         self.selected_module = ''
@@ -45,35 +45,36 @@ class TensorboardWriter():
             self.tag_mode_exceptions = {'add_histogram', 'add_embedding'}
             self.timer = datetime.now()
 
-        def set_step(self, step, mode='train'):
-            self.mode = mode
-            self.step = step
-            if step == 0:
-                self.timer = datetime.now()
-            else:
-                duration = datetime.now() - self.timer
-                self.add_scalar('steps_per_sec', 1 / duration.total_seconds())
-                self.timer = datetime.now()
+    def set_step(self, step, mode='train'):
+        self.mode = mode
+        self.step = step
+        if step == 0:
+            self.timer = datetime.now()
+        else:
+            duration = datetime.now() - self.timer
+            self.add_scalar('steps_per_sec', 1 / duration.total_seconds())
+            self.timer = datetime.now()
 
-        def __getattr__(self, name):
-            """
-            If visualization is asked, return add_data() methods of tensorboard with additional information (step, tag)
-            added.
-            Otherwise, return a blank function handle that does nothing
-            """
-            if name in self.tb_writer_ftns:
-                add_data = getattr(self.writer, name, None)
+    def __getattr__(self, name):
+        """
+        If visualization is asked, return add_data() methods of tensorboard with additional information (step, tag)
+        added.
+        Otherwise, return a blank function handle that does nothing
+        """
+        if name in self.tb_writer_ftns:
+            add_data = getattr(self.writer, name, None)
 
-                def wrapper(tag, data, *args, **kwargs):
-                    if add_data is not None:
-                        # add mode(train/valid) tag
-                        if name not in self.tg_mode_exceptions:
-                            tag = '{}/{}'.format(tag, self.mode)
-                        add_data(tag, data, self.step, *args, **kwargs)
-                return wrapper
-            else:
-                # default action for returning methods defined in this class, set_step() for instance
-                try:
-                    attr = object.__getattr__(name)
-                except AttributeError:
-                    raise AttributeError("type object '{}' has no attribute '{}'".format(self.selected_module, name))
+            def wrapper(tag, data, *args, **kwargs):
+                if add_data is not None:
+                    # add mode(train/valid) tag
+                    if name not in self.tg_mode_exceptions:
+                        tag = '{}/{}'.format(tag, self.mode)
+                    add_data(tag, data, self.step, *args, **kwargs)
+            return wrapper
+        else:
+            # default action for returning methods defined in this class, set_step() for instance
+            try:
+                attr = object.__getattribute__(name)
+            except AttributeError:
+                raise AttributeError("type object '{}' has no attribute '{}'".format(self.selected_module, name))
+            return attr
