3607b9397f21c03b6aab2c8a52ca989dddd987a2
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-03-10 18:05:23 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ The setup script for pip. Allows for `pip install -e .` installation.
 
 from setuptools import setup, find_packages
 
-requirements = ['numpy', 'matplotlib', 'torch', 'h5py', 'PyYAML']
+requirements = ['numpy', 'matplotlib', 'torch', 'h5py', 'PyYAML', 'torchvision']
 setup_requirements = []
 tests_requirements = ['pytest']
 
