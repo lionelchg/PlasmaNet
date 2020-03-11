abf869f19534fa52e6f1c54cc42c669dbdba12e5
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-03-11 17:56:04 +0100

    New config file, remove old trainer and validator, change metric name

--- PlasmaNet/model/metric.py
+++ PlasmaNet/model/metric.py
@@ -9,16 +9,6 @@
 import torch
 
 
-def accuracy(output, target):
-    """ Computes accuracy of the current epoch. """
-    with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
-
-
 def residual(output, target):
     """ Computes the residual of the current epoch. """
     with torch.no_grad():
@@ -26,7 +16,7 @@ def residual(output, target):
     return res
 
 
-def normed_residual(output, target):
+def l2_norm(output, target):
     """ Computes the L2 norm of the residual of the currend epoch. """
     with torch.no_grad():
         res = torch.sum((output - target) ** 2)
