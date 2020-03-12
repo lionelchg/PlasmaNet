798b9d89f9d9c35b5208746f2eeadc28fa53b29d
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-03-12 15:28:15 +0100

    Change matplotlib backend

--- PlasmaNet/trainer/trainer.py
+++ PlasmaNet/trainer/trainer.py
@@ -11,7 +11,9 @@ import torch
 from torchvision.utils import make_grid
 from ..base import BaseTrainer
 from ..utils import inf_loop, MetricTracker
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.use('Agg')
 
 
 class Trainer(BaseTrainer):
