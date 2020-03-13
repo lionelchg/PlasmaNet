da7fe3708c3f1050ab4e6c77e6c664d30c48a80c
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-03-13 11:38:50 +0100

    Add ComposedLoss and rework losses implementation

--- PlasmaNet/base/base_loss.py
+++ PlasmaNet/base/base_loss.py
@@ -19,12 +19,22 @@ class BaseLoss(_Loss):
     def __init__(self):
         super().__init__()
         self._require_input_data = False
+        self.result = None
+        self.loss_list = ['']
 
     @abstractmethod
-    def forward(self, output, target, **kwargs):
+    def _forward(self, output, target, **kwargs):
         """ Computes the loss (implemented as __call__ in nn.Module, from which it inherits). """
         raise NotImplementedError
+
+    def forward(self, output, target, **kwargs):
+        self.result = self._forward(output, target, **kwargs)
+        return self.result
 
     def require_input_data(self):
         """ Check whether forward method needs the data (here rhs) for computation (e.g. for laplacian loss). """
         return self._require_input_data
+
+    def log(self):
+        """ Returns last result"""
+        return {'loss': self.result}
--- PlasmaNet/model/loss.py
+++ PlasmaNet/model/loss.py
@@ -6,25 +6,27 @@
 #                                                                                                                      #
 ########################################################################################################################
 
+import torch
 from ..base.base_loss import BaseLoss
 import torch.nn.functional as F
 from ..operators.laplacian import laplacian as lapl
 from ..operators.gradient import gradient_scalar
+import sys
 
 
 class InsideLoss(BaseLoss):
     """ Computes the weighted MSELoss of the interior of the domain (excluding boundaries). """
-    def __init__(self, inside_weight):
+    def __init__(self, inside_weight, **_):
         super().__init__()
         self.weight = inside_weight
 
-    def forward(self, output, target, **kwargs):
+    def _forward(self, output, target, **kwargs):
         return F.mse_loss(output[:, 0, 1:-1, 1:-1], target[:, 0, 1:-1, 1:-1]) * self.weight
 
 
 class LaplacianLoss(BaseLoss):
     """ A Laplacian loss function on the inside of the domain (excluding boundaries). """
-    def __init__(self, lapl_weight, dx, dy):
+    def __init__(self, lapl_weight, dx, dy, **_):
         super().__init__()
         self.weight = lapl_weight
         self.dx = dx
@@ -34,14 +36,14 @@ class LaplacianLoss(BaseLoss):
             self.dy = eval(dy)
         self._require_input_data = True  # Need rhs for computation
 
-    def forward(self, output, target, data=None, target_norm=1., data_norm=1., **kwargs):
+    def _forward(self, output, target, data=None, target_norm=1., data_norm=1., **_):
         laplacian = lapl(output * target_norm / data_norm, self.dx, self.dy)
         return F.mse_loss(laplacian[:, 0, 1:-1, 1:-1], - data[:, 0, 1:-1, 1:-1]) * self.weight
 
 
 class ElectricLoss(BaseLoss):
     """ Loss function on the electric field. """
-    def __init__(self, elec_weight, dx, dy):
+    def __init__(self, elec_weight, dx, dy, **_):
         super().__init__()
         self.weight = elec_weight
         self.dx = dx
@@ -51,7 +53,7 @@ class ElectricLoss(BaseLoss):
             self.dy = eval(dy)
         self._require_input_data = False
 
-    def forward(self, output, target, **kwargs):
+    def _forward(self, output, target, **_):
         elec_output = gradient_scalar(output, self.dx, self.dy)
         elec_target = gradient_scalar(target, self.dx, self.dy)
         return F.mse_loss(elec_output, elec_target) * self.weight
@@ -59,11 +61,11 @@ class ElectricLoss(BaseLoss):
 
 class DirichletBoundaryLoss(BaseLoss):
     """ Loss for Dirichlet boundaries. """
-    def __init__(self, bound_weight):
+    def __init__(self, bound_weight, **_):
         super().__init__()
         self.weight = bound_weight
 
-    def forward(self, output, target, **kwargs):
+    def _forward(self, output, target, **_):
         bnd_loss = F.mse_loss(output[:, 0, 0, :], target[:, 0, 0, :])
         bnd_loss += F.mse_loss(output[:, 0, -1, :], target[:, 0, -1, :])
         bnd_loss += F.mse_loss(output[:, 0, :, 0], target[:, 0, :, 0])
@@ -73,7 +75,7 @@ class DirichletBoundaryLoss(BaseLoss):
 
 class NeumannBoundaryLoss(BaseLoss):
     """ Loss for Neumann boundaries. """
-    def __init__(self, bound_weight, dx, dy):
+    def __init__(self, bound_weight, dx, dy, **_):
         super().__init__()
         self.weight = bound_weight
         self.dx = dx
@@ -82,7 +84,7 @@ class NeumannBoundaryLoss(BaseLoss):
             self.dx = eval(dx)
             self.dy = eval(dy)
 
-    def forward(self, output, target, **kwargs):
+    def _forward(self, output, target, **_):
         # Compute electric field
         grad_output = gradient_scalar(output, self.dx, self.dy)
         grad_target = gradient_scalar(target, self.dx, self.dy)
@@ -96,20 +98,34 @@ class NeumannBoundaryLoss(BaseLoss):
 
 class ComposedLoss(BaseLoss):
     """
-    Class for loss composition.
-    TODO: pass list of classes as argument? With list of object, to avoid manually listing losses
+    Class for loss composition, with list of losses to compose as argument.
     """
-    def __init__(self, inside_weight, bound_weight, elec_weight, lapl_weight, dx, dy):
+    def __init__(self, loss_list, **kwargs):
+        """
+        Initializes the ComposedLoss with the loss specified in loss_list, and pass the arguments by dictionary
+        from the input file.
+        Therefore, the loss must have different argument name for the weight for example.
+        """
         super().__init__()
-        self.inside_loss = InsideLoss(inside_weight)
-        self.bound_loss = DirichletBoundaryLoss(bound_weight)
-        self.elec_loss = ElectricLoss(elec_weight, dx, dy)
-        self.lapl_loss = LaplacianLoss(lapl_weight, dx, dy)
-        self._require_input_data = True
+        # Initializes all losses in a list
+        self.loss_list = loss_list
+        self.losses = list()
+        for loss in self.loss_list:
+            try:
+                self.losses.append(getattr(sys.modules[__name__], loss)(**kwargs))  # Look for classes in this module
+            except TypeError as err:
+                print('while initializing {} class:'.format(loss))
+                raise type(err)('{} while initializing {} class.'.format(err, loss))
+        # Check if any loss requires data input
+        self._require_input_data = any([loss.require_input_data() for loss in self.losses])
+        # Store results for logger access
+        self.results = torch.zeros(len(self.loss_list))  # Use numpy to ensure floating-point sum
 
-    def forward(self, output, target, data=None, target_norm=1., data_norm=1.,  **kwargs):
-        composed_loss = self.inside_loss.forward(output, target)
-        composed_loss += self.bound_loss.forward(output, target)
-        composed_loss += self.elec_loss.forward(output, target)
-        composed_loss += self.lapl_loss.forward(output, target, data, target_norm, data_norm)
-        return composed_loss
+    def _forward(self, output, target, **kwargs):
+        for i, loss in enumerate(self.losses):
+            self.results[i] = loss(output, target, **kwargs)
+        return self.results.sum()
+
+    def log(self):
+        """ Returns log of each loss independently as a dict() with loss names keys and the associated torch values. """
+        return {loss_name: result.item() for loss_name, result in zip(self.loss_list, self.results)}
