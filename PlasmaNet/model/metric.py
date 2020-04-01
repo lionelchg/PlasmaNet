af0eb39419164d70a2b034e150d833ccf0a27ca9
Guillaume Bogopolsky <bogopolsky@cerfacs.fr>
2020-04-01 18:26:56 +0200

    Add pearsonr metric for correlation coefficient
    No need for nodal volume ponderation as it would be insensitive
    to ds, only to topology changes.

--- PlasmaNet/model/metric.py
+++ PlasmaNet/model/metric.py
@@ -10,21 +10,37 @@ import torch
 
 
 def residual(output, target, config):
-    """ Computes the residual of the current epoch. """
+    """ Computes the residual of the current batch. """
     with torch.no_grad():
-        res = config.ds / config.surface * torch.sum(torch.abs(output - target))
+        res = torch.sum(torch.abs(output - target) * config.voln) / config.surface
     return res
 
 
 def l2_norm(output, target, config):
-    """ Computes the L2 norm of the residual of the current epoch. """
+    """ Computes the L2 norm of the residual of the current batch. """
     with torch.no_grad():
-        res = torch.sqrt(config.ds / config.surface * torch.sum((output - target) ** 2))
+        res = torch.sqrt(torch.sum((output - target) ** 2 * config.voln) / config.surface)
     return res
 
 
 def inf_norm(output, target, config):
-    """ Computes the infinity norm of the residual of the current epoch. """
+    """ Computes the infinity norm of the residual of the current batch. """
     with torch.no_grad():
-        res = torch.max(torch.abs(output - target))
+        res = torch.max(torch.abs(output - target) * config.voln) / config.surface
     return res
+
+
+def ratio_avg(output, target, config):
+    """ Computes the average of the ratio between the output and target of the current batch. """
+    with torch.no_grad():
+        res = torch.mean(output / target)
+
+
+def pearsonr(output, target, config):
+    """ Computes the Pearson correlation coefficient between the output and target of the current batch. """
+    with torch.no_grad():
+        out_mean, tar_mean = output.mean(), target.mean()
+        out_centered, tar_centered = output - out_mean, target - tar_mean
+        r_num = torch.sum(out_centered * tar_centered)
+        r_den = torch.sqrt(torch.sum(out_centered ** 2) * torch.sum(tar_centered ** 2))
+        return r_num / r_den
