Add pearsonr2 metric, correct scaling for inf_norm

PlasmaNet/model/metric.py
@@ -12,14 +12,14 @@ from ..operators.gradient import gradient_scalar
 
 
 def residual(output, target, config):
-    """ Computes the residual of the current epoch. """
+    """ Computes the residual of the current batch. """
     with torch.no_grad():
         res = torch.sum(torch.abs(output - target)) / config.size ** 2 / config.batch_size
     return res
 
 
 def l2_norm(output, target, config):
-    """ Computes the L2 norm of the residual of the current epoch. """
+    """ Computes the L2 norm of the residual of the current batch. """
     with torch.no_grad():
         res = torch.sqrt(torch.sum((output - target) ** 2)) / config.size ** 2 / config.batch_size
     return res
@@ -28,7 +28,7 @@ def l2_norm(output, target, config):
 def inf_norm(output, target, config):
     """ Computes the infinity norm of the residual of the current batch. """
     with torch.no_grad():
-        res = torch.max(torch.abs(output - target)) * config.ds / config.surface
+        res = torch.max(torch.abs(output - target))
     return res
 
 
@@ -46,6 +46,11 @@ def pearsonr(output, target, config):
         r_num = torch.sum(out_centered * tar_centered)
         r_den = torch.sqrt(torch.sum(out_centered ** 2) * torch.sum(tar_centered ** 2))
         return r_num / r_den
+
+
+def pearsonr2(output, target, config):
+    """ Computes the squared Pearson correlation coefficient between the output and target of the current batch. """
+    return pearsonr(output, target, config) ** 2
 
 
 def rotational(output, target, config):