cd0986d0db97044d67e2717509035133b12d7c9a
lionelchg <cheng@cerfacs.fr>
2020-09-10 00:22:22 +0200

    Old changes from with run scripts

--- PoissonSolver/datasets/rhs_random.py
+++ PoissonSolver/datasets/rhs_random.py
@@ -48,7 +48,7 @@ A = laplace_square_matrix(n_points)
 ni0 = 1e16
 
 # Directories declaration and creation if necessary
-casename = f'{n_points:d}x{n_points}/random_{n_res:d}_train/'
+casename = f'{n_points:d}x{n_points}/random_{n_res:d}_test/'
 if device == 'mac':
     data_dir = 'rhs/' + casename
     plot = True
