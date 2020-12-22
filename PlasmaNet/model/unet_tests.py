696c0985b1aa141efe6287a6d8a2ed27fa75c28a
lionelchg <cheng@cerfacs.fr>
2020-12-22 14:58:01 +0100

    MSUnet5 implemented

--- PlasmaNet/model/unet_tests.py
+++ PlasmaNet/model/unet_tests.py
@@ -376,3 +376,44 @@ class UNet6_big(BaseModel):
                                         mode='bilinear', align_corners=False), convN_1out), dim=1))
         final_out = self.final(convN_11out)
         return final_out
+
+class MSUNet5(BaseModel):
+    """
+    Define the network, merge between UNet and MSNet.
+    Only input when called is number of data (input) channels.
+        - Perform 5 levels of convolution
+        - When returning to the original size, concatenate output of matching sizes
+        - The smaller domains are upsampled to the desired size with the F.upsample function.
+    """
+    def __init__(self, data_channels):
+        super(MSUNet5, self).__init__()
+        self.convN_1 = _ConvBlockIn(1, 32, 32)
+        self.convN_2 = _ConvBlockDown(52, 32, 32)
+        self.convN_3 = _ConvBlockDown(52, 32, 32)
+        self.convN_4 = _ConvBlockDown(52, 48, 62)
+        self.convN_5 = _ConvBlockDown(92, 60, 62)
+        self.convN_6 = _ConvBlockUp(124, 64, 64)
+        self.convN_7 = _ConvBlockUp(96, 64, 64)
+        self.convN_8 = _ConvBlockUp(96, 32, 32)
+        self.convN_9 = _ConvBlockUp(64, 32, 32)
+        self.final = _ConvBlockOut(32, 1)
+        
+    def forward(self, x):
+        convN_1out = self.convN_1(x)
+        convN_2out = self.convN_2(torch.cat((x.repeat(1, 20, 1, 1), convN_1out), dim=1))
+        convN_3out = self.convN_3(torch.cat((F.interpolate(x.repeat(1, 20, 1, 1), size=convN_2out[0, 0].shape,
+                                        mode='bilinear', align_corners=False), convN_2out), dim=1))
+        convN_4out = self.convN_4(torch.cat((F.interpolate(x.repeat(1, 20, 1, 1), size=convN_3out[0, 0].shape,
+                                        mode='bilinear', align_corners=False), convN_3out), dim=1))
+        convN_5out = self.convN_5(torch.cat((F.interpolate(x.repeat(1, 30, 1, 1), size=convN_4out[0, 0].shape,
+                                        mode='bilinear', align_corners=False), convN_4out), dim=1))
+        convN_6out = self.convN_6(torch.cat((F.interpolate(convN_5out, size=convN_4out[0, 0].shape, 
+                                        mode='bilinear', align_corners=False), convN_4out), dim=1))
+        convN_7out = self.convN_7(torch.cat((F.interpolate(convN_6out, size=convN_3out[0, 0].shape, 
+                                        mode='bilinear', align_corners=False), convN_3out), dim=1))
+        convN_8out = self.convN_8(torch.cat((F.interpolate(convN_7out, size=convN_2out[0, 0].shape, 
+                                        mode='bilinear', align_corners=False), convN_2out), dim=1))
+        convN_9out = self.convN_9(torch.cat((F.interpolate(convN_8out, size=convN_1out[0, 0].shape, 
+                                        mode='bilinear', align_corners=False), convN_1out), dim=1))
+        final_out = self.final(convN_9out)
+        return final_out
