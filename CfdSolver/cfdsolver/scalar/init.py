82883fd56e94064bf49234a22d7d30fdb494b38c
lionelchg <cheng@cerfacs.fr>
2021-02-01 13:09:27 +0100

    Other minor updates and modifications of config files

--- CfdSolver/cfdsolver/scalar/init.py
+++ CfdSolver/cfdsolver/scalar/init.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import interpolate
 
 def gaussian(x, y, amplitude, x0, y0, sigma_x, sigma_y):
     """ Gaussian function """
@@ -32,3 +33,13 @@ def gaussians(x, y, params):
     for index in range(ngauss):
         profile += gaussian(x, y, *params[index, :])
     return profile
+
+def random(X, Y, amplitude, n_res, sigma):
+    xmin, xmax, npts = np.min(X), np.max(X), len(X[0, :])
+    n_lower = int(npts / n_res)
+    x_lower, y_lower = np.linspace(xmin, xmax, n_lower), np.linspace(xmin, xmax, n_lower)
+    z_lower = amplitude * (2 * np.random.random((n_lower, n_lower)) - 1)
+    f = interpolate.interp2d(x_lower, y_lower, z_lower, kind='cubic')
+    x, y = np.linspace(xmin, xmax, npts), np.linspace(xmin, xmax, npts)
+    x0, y0 = (xmax - xmin) / 2, (xmax - xmin) / 2
+    return f(x, y) * np.exp(-((X - x0)**2 / sigma**2 + (Y - y0)**2 / sigma**2))
