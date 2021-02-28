3fbdb348ffe0d0c0f62aeca3ae64d09c0e7373b8
Lionel Cheng <cheng@cerfacs.fr>
2021-02-28 18:36:28 +0100

    Start refactoring the whole code
    Have the three former libraries (plasmanet, cfdsolver and poissonsolver)
    all under one library.
    Creates a common plotting library for all these libraries (no copy/paste
    between libraries for that matter).
    This commit just moved the file, all the libraries need to be debugged

---
+++ PlasmaNet/nnet/__init__.py
@@ -0,0 +1,26 @@
+"""
+PlasmaNet: Solving the electrostatic Poisson equation for plasma simulations
+
+Guillaume Bogopolsky, Lionel Cheng, Ekhi Ajuria
+CERFACS
+"""
+
+__author__ = 'Guillaume Bogopolsky, Lionel Cheng, Ekhi Ajuria'
+__mail__ = ''
+__version__ = '0.1'
+
+from .data import data_loaders
+from .model import loss, metric, multiscalenet, dirichletnet, unet
+from .trainer.trainer import Trainer
+from .parse_config import ConfigParser
+
+__ALL__ = [
+    data_loaders,
+    loss,
+    metric,
+    multiscalenet,
+    dirichletnet,
+    unet,
+    Trainer,
+    ConfigParser
+]
