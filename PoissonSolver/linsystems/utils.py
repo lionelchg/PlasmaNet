a0010eb97a4229d02464b5346bf0dd149cc0f7dd
Lionel Cheng <cheng@cerfacs.fr>
2021-02-28 23:52:23 +0100

    Cleaning of directories and file update
    Remove filters and linsystems directories
    Update superposition test on poisson

--- PoissonSolver/linsystems/utils.py
+++
@@ -1,8 +0,0 @@
-def print_mat(A, matname):
-    print(f'Matrice {matname}')
-    n = len(A[:, 0])
-    for i in range(n):
-        row = ''
-        for j in range(n):
-            row += f'{A[i, j]:>6.2f} '
-        print(row)
