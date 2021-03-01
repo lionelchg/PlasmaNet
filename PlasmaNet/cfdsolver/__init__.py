8e525be490ea1f170c60ecab02cb06a879752b61
Lionel Cheng <cheng@cerfacs.fr>
2021-03-01 14:20:22 +0100

    Refactor of convective vortex

--- PlasmaNet/cfdsolver/__init__.py
+++ PlasmaNet/cfdsolver/__init__.py
@@ -9,14 +9,4 @@ __author__ = 'Lionel Cheng'
 __mail__ = ''
 __version__ = '0.5'
 
-from .euler.euler import Euler
-from .euler.plasma import PlasmaEuler
-from .scalar.scalar import ScalarTransport
-from .scalar.streamer import StreamerMorrow
-
-__ALL__ = [
-    Euler,
-    ScalarTransport,
-    StreamerMorrow,
-    PlasmaEuler
-]
+__ALL__ = []
--- PlasmaNet/cfdsolver/euler/euler.py
+++ PlasmaNet/cfdsolver/euler/euler.py
@@ -10,6 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as co
 import copy
+import yaml
+import argparse
 from numba import njit
 
 from ..base.base_plot import plot_ax_scalar, plot_ax_scalar_1D
@@ -114,7 +116,64 @@ class Euler(BaseSim):
         fig.tight_layout(rect=[0, 0.03, 1, 0.97])
         fig.savefig(self.fig_dir + f'1D_{self.number:04d}', bbox_inches='tight')
         plt.close(fig)
+    
+    @classmethod
+    def run(cls, config):
+        """ Main function containing initialization, temporal loop and outputs. Takes a config dict as input. """
 
+        sim = cls(config)
+
+        # Convective vortex parameters and initialization
+        x0, y0 = 0, 0
+        u0, v0 = 2, 0
+        rho0, p0 = 1, 1 / sim.gamma
+        alpha, K = 1, 5
+        a0 = np.sqrt(sim.gamma * p0 / rho0)
+        T0 = 1 / sim.gamma / sim.r
+        covo(sim.X, sim.Y, x0, y0, u0, v0, rho0, p0, T0, alpha, K, sim.gamma, sim.r, 0, sim.U)
+
+        # Print header to sum up the parameters
+        if sim.verbose:
+            sim.print_init()
+
+        # Iterations
+        for it in range(1, sim.nit + 1):
+            # Update of the residual to zero
+            sim.res[:], sim.res_c[:] = 0, 0
+
+            # Compute euler fluxes
+            sim.compute_flux()
+
+            # Calculate timestep based on the maximum value of u + ss
+            sim.compute_timestep()
+
+            # Compute residuals in cell-vertex method
+            sim.compute_res()
+
+            # boundary conditions
+            sim.impose_bc_euler()
+            
+            # Apply residual
+            sim.update_res()
+
+            # Post processing
+            sim.postproc(it)
+            
+def covo(x, y, x0, y0, u0, v0, rho0, p0, T0, alpha, K, gamma, r, t, U):
+    """ Initialize isentropic convective vortex as given by idolikecfd chap 7. """
+    xbar = x - x0 - u0 * t
+    ybar = y - y0 - v0 * t
+    rbar = np.sqrt(xbar**2 + ybar**2)
+    u = u0 - K / (2 * np.pi) * ybar * np.exp(alpha * (1 - rbar**2) / 2)
+    v = v0 + K / (2 * np.pi) * xbar * np.exp(alpha * (1 - rbar**2) / 2)
+    T = T0 - K**2 * (gamma - 1) / (8 * alpha * np.pi**2 * gamma * r) * np.exp(alpha * (1 - rbar**2))
+    rho = rho0 * (T / T0)**(1 / (gamma - 1))
+    p = p0 * (T / T0)**(gamma / (gamma - 1))
+    # Define conservative variables
+    U[0] = rho                                          # density
+    U[1] = rho * u                                      # momentum along x
+    U[2] = rho * v                                      # momentum along y
+    U[3] = rho / 2 * (u**2 + v**2) + p / (gamma - 1)    # total energy with closure on internal energy
 
 @njit(cache=True)
 def compute_timestep(cfl, dx, U, press, gamma):
@@ -206,3 +265,14 @@ def compute_res(U, F, dt, volc, snc, ncx, ncy, gamma, ndim, nvert, res, res_c, U
             res[:, j, i + 1] -= 0.5 * ( dF_c[:, 0] * snc[1, 0] + dF_c[:, 1] * snc[1, 1] )
             res[:, j + 1, i + 1] -= 0.5 * ( dF_c[:, 0] * snc[2, 0] + dF_c[:, 1] * snc[2, 1] )
             res[:, j + 1, i] -= 0.5 * ( dF_c[:, 0] * snc[3, 0] + dF_c[:, 1] * snc[3, 1] )
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='Euler run')
+    args.add_argument('-c', '--config', type=str,
+                        help='Config filename', required=True)
+    args = args.parse_args()
+
+    with open(args.config, 'r') as yaml_stream:
+        cfg = yaml.safe_load(yaml_stream)
+
+    Euler.run(cfg)
