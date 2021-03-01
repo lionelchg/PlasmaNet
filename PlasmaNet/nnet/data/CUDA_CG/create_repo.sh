4a6c1b4f0d06c1e1befa7af86ecb363d49eb6762
Lionel Cheng <cheng@cerfacs.fr>
2021-03-01 15:05:34 +0100

    Delete unused repositories in PlasmaNet/nnet/

--- PlasmaNet/nnet/data/CUDA_CG/create_repo.sh
+++
@@ -1,4 +0,0 @@
-mkdir figures figures/dirichlet figures/hills figures/operators figures/superposition
-mkdir datasets
-mkdir datasets/dirichlet datasets/dirichlet/64x64 datasets/dirichlet/128x128
-mkdir datasets/rhs datasets/rhs/64x64 datasets/rhs/128x128
