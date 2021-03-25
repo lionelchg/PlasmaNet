6098b8e10aa055bbeee9f8dc51ad1f1204821b8f
Lionel Cheng <cheng@cerfacs.fr>
2021-03-25 12:16:23 +0100

    Update modes for inclusion of Neumann

--- PoissonSolver/analytical/modes.py
+++ PoissonSolver/analytical/modes.py
@@ -1,15 +1,69 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from PlasmaNet.common.plot import plot_ax_scalar
 from PlasmaNet.common.utils import create_dir
 
 
-def mode(X, Y, Lx, Ly, n, m):
-    return np.sin(n * np.pi * X / Lx) * np.sin(m * np.pi * Y / Ly)
+def dirichlet_mode(x:np.ndarray, Lx:float, n:int) -> np.ndarray:
+    """ Mode for 1D full Dirichlet Poisson problem
 
+    :param x: direction vector
+    :type x: np.ndarray
+    :param Lx: length of the domain in the direction
+    :type Lx: float
+    :param n: mode number
+    :type n: int
+    :return: mode of number n
+    :rtype: np.ndarray
+    """
+    return np.sin(n * np.pi * x / Lx)
+
+def neumann_mode(x:np.ndarray, Lx:float, n:int) -> np.ndarray:
+    """ Mode for 1D full Neumann Poisson problem
+
+    :param x: direction vector
+    :type x: np.ndarray
+    :param Lx: length of the domain in the direction
+    :type Lx: float
+    :param n: mode number
+    :type n: int
+    :return: mode of number n
+    :rtype: np.ndarray
+    """
+    return np.cos(n * np.pi * x / Lx)
+
+def mixed_mode_ldrn(x:np.ndarray, Lx:float, n:int) -> np.ndarray:
+    """ Mode for 1D left Dirichlet - right Neumann Poisson problem
+
+    :param x: direction vector
+    :type x: np.ndarray
+    :param Lx: length of the domain in the direction
+    :type Lx: float
+    :param n: mode number
+    :type n: int
+    :return: mode of number n
+    :rtype: np.ndarray
+    """
+    return np.sin((n + 0.5) * np.pi * x / Lx)
+
+def mixed_mode_lnrd(x:np.ndarray, Lx:float, n:int) -> np.ndarray:
+    """ Mode for 1D left Neumann - right Dirichlet Poisson problem
+
+    :param x: direction vector
+    :type x: np.ndarray
+    :param Lx: length of the domain in the direction
+    :type Lx: float
+    :param n: mode number
+    :type n: int
+    :return: mode of number n
+    :rtype: np.ndarray
+    """
+    return np.cos((n + 0.5) * np.pi * x / Lx)
 
 if __name__=='__main__':
+    sns.set_context('notebook', font_scale=0.9, rc={"lines.linewidth": 1.5})
     fig_dir = 'figures/modes/'
     create_dir(fig_dir)
 
@@ -22,10 +76,44 @@ if __name__=='__main__':
 
     X, Y = np.meshgrid(x, y)
     N, M = 3, 3
-    fig, axes = plt.subplots(ncols=N, nrows=M, figsize=(20, 20))
+
+    # Full dirichlet modes
+    fig, axes = plt.subplots(ncols=N, nrows=M, figsize=(10, 10))
     for n in range(1, N + 1):
         for m in range(1, M + 1):
             plot_ax_scalar(fig, axes[n - 1][m - 1], X, Y, 
-            mode(X, Y, Lx, Ly, n, m), 'n = %d m = %d' % (n, m), cbar=False)
+            dirichlet_mode(X, Lx, n) * dirichlet_mode(Y, Ly, m), 'n = %d m = %d' % (n, m), cbar=False)
     plt.tight_layout()
-    plt.savefig(fig_dir + 'modes_%d_%d' % (N, M))
+    plt.savefig(fig_dir + 'dirichlet_%d_%d' % (N, M))
+    plt.close()
+
+    # Full neumann modes
+    fig, axes = plt.subplots(ncols=N, nrows=M, figsize=(10, 10))
+    for n in range(1, N + 1):
+        for m in range(1, M + 1):
+            plot_ax_scalar(fig, axes[n - 1][m - 1], X, Y, 
+            neumann_mode(X, Lx, n) * neumann_mode(Y, Ly, m), 'n = %d m = %d' % (n, m), cbar=False)
+    plt.tight_layout()
+    plt.savefig(fig_dir + 'neumann_%d_%d' % (N, M))
+    plt.close()
+
+    # Modes for dirichlet at left and bottom, neumann at right and top boundaries
+    fig, axes = plt.subplots(ncols=N, nrows=M, figsize=(10, 10))
+    for n in range(N):
+        for m in range(M):
+            plot_ax_scalar(fig, axes[n - 1][m - 1], X, Y, 
+            mixed_mode_ldrn(X, Lx, n) * mixed_mode_ldrn(Y, Ly, m), 'n = %d m = %d' % (n, m), cbar=False)
+    plt.tight_layout()
+    plt.savefig(fig_dir + 'mixed_ldrn_%d_%d' % (N, M))
+    plt.close()
+
+    # Modes for Neumann at left and bottom, Dirichlet at right and top boundaries
+    fig, axes = plt.subplots(ncols=N, nrows=M, figsize=(10, 10))
+    for n in range(N):
+        for m in range(M):
+            plot_ax_scalar(fig, axes[n - 1][m - 1], X, Y, 
+            mixed_mode_lnrd(X, Lx, n) * mixed_mode_lnrd(Y, Ly, m), 'n = %d m = %d' % (n, m), cbar=False)
+    plt.tight_layout()
+    plt.savefig(fig_dir + 'mixed_lnrd_%d_%d' % (N, M))
+    plt.close()
+
