200ac8ca259dcc2f05e7054242e6dffa5efb9f03
Lionel Cheng <cheng@cerfacs.fr>
2021-04-08 16:20:33 +0200

    Update of the nparams scripts

--- NNet/archs/nparams.py
+++ NNet/archs/nparams.py
@@ -1,12 +1,18 @@
+import argparse
 import yaml
 import PlasmaNet.nnet.model as model
 
 if __name__ == '__main__':
-    with open('unets.yml') as yaml_stream:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='Config filename')
+    args = parser.parse_args()
+
+    with open(args.config) as yaml_stream:
         cfg = yaml.safe_load(yaml_stream)
     
     for key, value in cfg.items():
         print(f'Architecture: {key}')
         value['args']['input_res'] = 101
+        value['args']['up_type'] = 'deconvolution'
         tmp_model = getattr(model, value['type'])(**value['args'])
-        print(tmp_model)
+        print(f'Number of trainable parameters: {tmp_model.nparams:d}\n')
