7204d4faf7da1797b60caaf2c1744f9a7c626979
Lionel Cheng <cheng@cerfacs.fr>
2021-05-26 11:26:00 +0200

    Add method for concatenating datasets in eval

--- PlasmaNet/poissonsolver/eval_datasets.py
+++ PlasmaNet/poissonsolver/eval_datasets.py
@@ -12,14 +12,34 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
+import shutil
 
 # From PlasmaNet
 from PlasmaNet.poissonsolver.network import PoissonNetwork
+
+def concatenate_ds(datasets: dict, output_dsname: Path):
+    """ Concatenate the datasets specified in dict into a single dataset """
+    physical_rhs_list = list()
+    potential_list = list()
+    for ds_name, ds_loc in datasets.items():
+        tmp_rhs = np.load(Path(ds_loc) / 'physical_rhs.npy')
+        tmp_pot = np.load(Path(ds_loc) / 'potential.npy')
+        physical_rhs_list.append(tmp_rhs)
+        potential_list.append(tmp_pot)
+    
+    physical_rhs_list = np.concatenate(tuple(physical_rhs_list), axis=0)
+    potential_list = np.concatenate(tuple(potential_list), axis=0)
+    np.save(output_dsname / 'physical_rhs', physical_rhs_list)
+    np.save(output_dsname / 'potential', potential_list)
 
 def main():
     args = argparse.ArgumentParser(description='PoissonNetwork runs')
     args.add_argument('-c', '--config', default=None, type=str,
                       help='Config file path (default: None)')
+    args.add_argument('-m', '--mode', default='separate', type=str,
+                      help='Run mode of the dataset (either combined or separated)')
+    args.add_argument('-fn', '--figname', default='metrics', type=str,
+                      help='Name of the figure in the casename directory')
     args = args.parse_args()
 
     with open(args.config, 'r') as yaml_stream:
@@ -29,14 +49,21 @@ def main():
     networks_cfg = config['networks']
     n_networks = len(networks_cfg)
 
+    # datadir
+    data_dir = Path(config['network']['casename'])
+    data_dir.mkdir(parents=True, exist_ok=True)
+
+    # if combined mode then concatenate datasets
+    if args.mode == 'combined':
+        output_dsname = data_dir / 'combined'
+        output_dsname.mkdir(parents=True, exist_ok=True)
+        concatenate_ds(config['datasets'], output_dsname)
+        config['datasets'] = {'combined': output_dsname}
+
     # datasets studied
     datasets = config['datasets']
     ndatasets = len(datasets)
     metrics_ds = dict()
-
-    # datadir
-    data_dir = Path(config['network']['casename'])
-    data_dir.mkdir(parents=True, exist_ok=True)
 
     # Evaluate on the datasets specified in config for each network
     for nn_name, nn_cfg in networks_cfg.items():
@@ -72,7 +99,12 @@ def main():
         metrics[metric_name] = tmp_df
     
     fig.tight_layout()
-    fig.savefig(data_dir / 'metrics', bbox_inches='tight')
+    fig.savefig(data_dir / args.figname, bbox_inches='tight')
+    plt.close()
+
+    # Delete combined dataset 
+    if args.mode == 'combined':
+        shutil.rmtree(output_dsname)
 
 if __name__ == '__main__':
     main()
