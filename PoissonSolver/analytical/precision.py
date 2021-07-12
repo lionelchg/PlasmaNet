9f08fe35dd4a359271a5bff5fdcdef3fc1a22425
Lionel Cheng <cheng@cerfacs.fr>
2021-07-12 23:51:43 +0200

    Improve analytical/precision.py script

--- PoissonSolver/analytical/precision.py
+++ PoissonSolver/analytical/precision.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yaml
 import scipy.constants as co
+from scipy import interpolate
 from pathlib import Path
 from time import perf_counter, time
 
@@ -17,35 +18,11 @@ from PlasmaNet.poissonsolver.poisson import PoissonLinSystem
 from PlasmaNet.poissonsolver.analytical import PoissonAnalytical
 import PlasmaNet.common.profiles as pf
 
-
-if __name__ == '__main__':
-    fig_dir = Path('figures/precision/')
+def precision_runs(fig_dir, physical_rhs, nsolves, nmax_modes):
+    # Creation of figures directory
     fig_dir.mkdir(parents=True, exist_ok=True)
-        
-    with open('poisson_ls_xy.yml') as yaml_stream:
-        cfg = yaml.safe_load(yaml_stream)
-
-    poisson = PoissonLinSystem(cfg)
-
-    zeros_x, zeros_y = np.zeros(poisson.nnx), np.zeros(poisson.nny)
-    ones_x, ones_y = np.ones(poisson.nnx), np.ones(poisson.nny)
-    pot_bcs = {'left':zeros_y, 'right':zeros_y, 'bottom':zeros_x, 'top':zeros_x}
-
-    # creating the rhs
-    ni0 = 1e16
-    sigma_x, sigma_y = 1e-3, 1e-3
-    x0, y0 = 0.6e-2, 0.5e-2
-    x01, y01 = 0.4e-2, 0.5e-2    
-    # params_gauss = [ni0, x0, y0, sigma_x, sigma_y, ni0, x01, y01, sigma_x, sigma_y]
-    params_gauss = [ni0, x0, y0, sigma_x, sigma_y, 
-                    ni0, x01, y01, 2e-3, 2e-3,
-                    8e15, 0.4e-2, 0.7e-2, 1e-3, 1e-3]
-
-    # interior rhs
-    physical_rhs = pf.gaussians(poisson.X, poisson.Y, params_gauss)
 
     # Number of solves
-    nsolves = 5
     print(f'{nsolves:d} solves')
 
     # linear system solve and solution norm
@@ -62,8 +39,6 @@ if __name__ == '__main__':
     poisson_series = PoissonAnalytical(cfg)
 
     # Variation of number of modes
-    # nmax_modes = [2, 4, 6, 8, 10, 12, 14, 16]
-    nmax_modes = [5, 10, 15, 20]
     error_modes = np.zeros((2, len(nmax_modes)))
 
     for inmax, nmax in enumerate(nmax_modes):
@@ -93,3 +68,41 @@ if __name__ == '__main__':
     ax.legend()
     ax.set_yscale('log')
     fig.savefig(fig_dir / 'errors')
+
+if __name__ == '__main__':
+    fig_dir = Path('figures/precision/')
+    fig_dir.mkdir(parents=True, exist_ok=True)
+        
+    with open('poisson_ls_xy.yml') as yaml_stream:
+        cfg = yaml.safe_load(yaml_stream)
+
+    poisson = PoissonLinSystem(cfg)
+
+    zeros_x, zeros_y = np.zeros(poisson.nnx), np.zeros(poisson.nny)
+    ones_x, ones_y = np.ones(poisson.nnx), np.ones(poisson.nny)
+    pot_bcs = {'left':zeros_y, 'right':zeros_y, 'bottom':zeros_x, 'top':zeros_x}
+
+    # modes studied
+    nmax_modes = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+    # creating the rhs
+    ni0 = 1e16
+    sigma_x, sigma_y = 1e-3, 1e-3
+    x0, y0 = 0.6e-2, 0.5e-2
+    x01, y01 = 0.4e-2, 0.5e-2    
+    params_gauss = [ni0, x0, y0, sigma_x, sigma_y, ni0, x01, y01, sigma_x, sigma_y]
+    # Two gaussians rhs
+    physical_rhs = pf.gaussians(poisson.X, poisson.Y, params_gauss) * co.e / co.epsilon_0
+    # run analytical solutions for physical_rhs
+    precision_runs(fig_dir / 'random_8', physical_rhs, 5, nmax_modes)
+
+    # creating random rhs
+    n_res_factor = 8
+    nnx_lower = int(poisson.nnx / n_res_factor)
+    nny_lower = int(poisson.nny / n_res_factor)
+    z_lower = 2 * np.random.random((nny_lower, nnx_lower)) - 1
+    x_lower, y_lower = np.linspace(poisson.xmin, poisson.xmax, nnx_lower), np.linspace(poisson.ymin, poisson.ymax, nny_lower)
+    f = interpolate.interp2d(x_lower, y_lower, z_lower, kind='cubic')
+    physical_rhs = f(poisson.x, poisson.y) * ni0 * co.e / co.epsilon_0
+    # run analytical solutions for physical_rhs
+    precision_runs(fig_dir / 'random_8', physical_rhs, 5, nmax_modes)
