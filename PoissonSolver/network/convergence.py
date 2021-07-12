c09328a48fb6d84f402a0c8affa0e1db0880b07c
Lionel Cheng <cheng@cerfacs.fr>
2021-07-12 14:45:42 +0200

    Update convergence of network plotting

--- PoissonSolver/network/convergence.py
+++ PoissonSolver/network/convergence.py
@@ -15,30 +15,31 @@ import argparse
 import yaml
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import scipy.constants as co
 import seaborn as sns
 from pathlib import Path
 
+# From PlasmaNet
 from PlasmaNet.poissonsolver.analytical import dirichlet_mode
-from PlasmaNet.poissonsolver.poisson import DatasetPoisson
 from PlasmaNet.poissonsolver.network import PoissonNetwork
-from PlasmaNet.common.profiles import gaussian
 from PlasmaNet.common.operators_numpy import grad
-from PlasmaNet.common.utils import create_dir
 
-sns.set_context('notebook', font_scale=1.0)
-
-
-def ax_prop(ax, title):
+def ax_prop(ax, ylabel):
     ax.grid(True)
     ax.set_xlabel(r'$n_\mathrm{pts}$')
-    ax.set_ylabel(r'$\varepsilon$')
+    ax.set_ylabel(ylabel)
     ax.set_xscale('log')
     ax.set_yscale('log')
-    ax.set_title(title)
-    ax.legend()
 
 if __name__ == '__main__':
+    # Plotting options for aesthetics
+    sns.set_context('notebook', font_scale=1.1)
+    lines_params = {'basic': {'linewidth': 2, 'markersize': 6}}
+    line_style = {'marker':'o'}
+    mpl.rc('lines', **lines_params['basic'])
+    # mpl.rc('savefig', format='pdf')
+
     args = argparse.ArgumentParser(description='PoissonNetwork runs')
     args.add_argument('-c', '--config', default=None, type=str,
                       help='Config file path (default: None)')
@@ -54,7 +55,9 @@ if __name__ == '__main__':
     # creating the rhs amplitude and the studied modes
     ni0 = 1e+11
     rhs0 = ni0 * co.e / co.epsilon_0
-    modes = ((1, 1), (1, 2), (2, 1), (2, 2), (4, 4), (6, 6), (10, 10))
+    modes = ((1, 1), (1, 2), (2, 1), (2, 2), (6, 6), (10, 10))
+    modes_style = [{'color': 'darkblue', 'marker': 'o'}, {'color': 'royalblue', 'marker':'o'}, {'color': 'darkblue', 'marker': '^'}, {'color': 'royalblue', 'marker': '^'}, {'color': 'firebrick', 'marker': 'o'},
+    {'color': 'firebrick', 'marker': '^'}]
     
     # Different studied resolutions
     nnxs = np.array([51, 101, 201, 401, 701, 1001, 2001])
@@ -83,8 +86,8 @@ if __name__ == '__main__':
             potential_th = physical_rhs / ((mode_n * np.pi / poisson.Lx)**2 + (mode_m * np.pi / poisson.Ly)**2)
             E_field_th = - grad(potential_th, poisson.dx, poisson.dy, nnx, nny)
             poisson.solve(physical_rhs)
-            if i_nnx == len(nnxs) - 1:
-                poisson.plot_1D2D(fig_dir / f'sol_{nnx}_{mode_n}_{mode_m}')
+            # if i_nnx == len(nnxs) - 1:
+            #     poisson.plot_1D2D(fig_dir / f'sol_{nnx}_{mode_n}_{mode_m}')
             for key in errors:
                 errors[key][f'({mode_n:d}, {mode_m:d})']['potential'][i_nnx] = getattr(poisson, f'{key}error_pot')(potential_th)
                 errors[key][f'({mode_n:d}, {mode_m:d})']['E_field'][i_nnx] = getattr(poisson, f'{key}error_E')(E_field_th)
@@ -95,12 +98,24 @@ if __name__ == '__main__':
     # Creation of L1, L2 and Linf error figs
     for error_kind in errors:
         fig, axes = plt.subplots(ncols=2, figsize=(8, 5))
-        for (mode_n, mode_m) in modes:
-            axes[0].plot(nnxs, errors[error_kind][f'({mode_n:d}, {mode_m:d})']['potential'], label=f'({mode_n:d}, {mode_m:d})')
-        ax_prop(axes[0], 'Potential')
-        for (mode_n, mode_m) in modes:
-            axes[1].plot(nnxs, errors[error_kind][f'({mode_n:d}, {mode_m:d})']['E_field'], label=f'({mode_n:d}, {mode_m:d})')
-        ax_prop(axes[1], 'Electric field')
-        fig.tight_layout()
-        fig.savefig(fig_dir / f'{error_kind}_error', bbox_inches='tight')
+        for imode, (mode_n, mode_m) in enumerate(modes):
+            axes[0].plot(nnxs, errors[error_kind][f'({mode_n:d}, {mode_m:d})']['potential'], label=f'({mode_n:d}, {mode_m:d})', **modes_style[imode])
+        ax_prop(axes[0], r'$\epsilon_\phi$')
+        for imode, (mode_n, mode_m) in enumerate(modes):
+            axes[1].plot(nnxs, errors[error_kind][f'({mode_n:d}, {mode_m:d})']['E_field'], label=f'({mode_n:d}, {mode_m:d})', **modes_style[imode])
+        ax_prop(axes[1], r'$\epsilon_\mathbf{E}$')
+
+        # Put global legend for both figures
+        handles, labels = axes[1].get_legend_handles_labels()
+        fig.legend(handles, labels, loc='center right', borderaxespad=0.1)
+        fig.subplots_adjust(right=0.82)
+
+        # Move left figure
+        tmp_pos = fig.axes[0].get_position()
+        tmp_pos.x0 -= 0.05
+        tmp_pos.x1 -= 0.05
+        fig.axes[0].set_position(tmp_pos)
+
+        # fig.tight_layout()
+        fig.savefig(fig_dir / f'{error_kind}_error.pdf', format='pdf', bbox_inches='tight')
         plt.close(fig)
